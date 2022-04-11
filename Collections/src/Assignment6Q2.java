d265e156681b7db677fc380a5e5cdeb910cc32c6
Ganeshd2812 <98973730+Ganeshd2812@users.noreply.github.com>
2022-04-11 13:00:30 +0530

    Add files via upload

---
+++ Collections/src/Assignment6Q2.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Assignment6Q2 {
+
+    public static void main(String[] args) {
+        HashSet stud = new HashSet();
+        stud.add("vikash");
+        stud.add("vishal");
+        stud.add("vickram");
+        stud.add("Raj");
+        stud.add("Murali");
+        stud.add("Sathya");
+        stud.add("Ponz");
+        stud.add("Skp");
+        stud.add("Sbk");
+        stud.add("ram");
+
+        System.out.println(stud);
+
+        System.out.println(stud.add("Rahul"));
+        System.out.println(stud);
+
+
+
+
+
+
+
+
+
+    }
+
+}
