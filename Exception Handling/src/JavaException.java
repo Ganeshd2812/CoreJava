472ba1e047ac4a4d6c944df90dc3c21dd7643d9e
Ganeshd2812 <98973730+Ganeshd2812@users.noreply.github.com>
2022-04-11 12:56:40 +0530

    Add files via upload

---
+++ Exception Handling/src/JavaException.java
@@ -0,0 +1,19 @@
+
+public class JavaException {
+
+    public static void main(String[] args) {
+        int a, b, c;
+        a = 10;
+        b = 0;
+        try {
+            c = a / b;
+            System.out.println(c);
+        } catch (ArithmeticException e) {
+
+            System.out.println("ArithmeticException Handled");
+
+
+        }
+    }
+}
+
