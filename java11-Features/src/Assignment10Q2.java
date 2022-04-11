bbad917fd10e3e87e50eaa607e0f21e8fe44655b
Ganeshd2812 <98973730+Ganeshd2812@users.noreply.github.com>
2022-04-11 13:04:46 +0530

    Add files via upload

---
+++ java11-Features/src/Assignment10Q2.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Assignment10Q2 {
+    //var x = 50;   // - 1
+    public static void main(String[] args)
+    {
+        /*
+         var<var> hs = new Arraylist<>(); // - 2
+         var<Integer> hs2 = new Arraylist<>(); // - 2
+         var x; //  - 3
+         var l1 = (i,j) -> (i/j) ; //  - 3
+        */
+        System.out.println(7);
+    }
+    //var demo(var a){return  ("Hello")}; // -4
+}
